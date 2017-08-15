3f6e3a495541fd7970f8b70c90d56dcbeef9a480
eliasdok <eliasdok@gmail.com>
2017-08-15 16:21:21 -0700

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -5,7 +5,7 @@ account_sid = "AC0f29b00ec845ce4a29c3ae8353ce8df7"
 auth_token = "b3321875a8085b99155049226a277b83"
 client = Client(account_sid, auth_token)
 client.messages.create(
-  to="+14156198294",
+  to="+XXXXXXXXXXX",
   from_="+12565769307",
   body="Tomorrow's forecast in Financial District, San Francisco is Clear",
   media_url="https://climacons.herokuapp.com/clear.png")
